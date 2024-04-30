041ea548eee6220135a64e5117d810cefc64988b
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-04-30 19:19:13 +0200

    AB#1078 Time Series API Mesh adapter can store TS data

---
+++ src/MeshAdapter/Configuration/MeshAdapterConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Meshmakers.Octo.MeshAdapter.Configuration;
+
+public class MeshAdapterConfiguration
+{
+    public string StreamDataConnectionString { get; set; }
+    
+    public MeshAdapterConfiguration()
+    {
+        StreamDataConnectionString = "Host=127.0.0.1;Username=crate;SSL Mode=Prefer";
+    }
+}

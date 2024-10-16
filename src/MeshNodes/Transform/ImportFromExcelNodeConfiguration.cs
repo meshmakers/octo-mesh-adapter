7ab4f531f3c5c07c3cdb4a760cf80154896907bd
Reimar Klammer <reimar.klammer@meshmakers.io>
2024-10-16 17:24:18 +0200

    AB#1481 Implementation of column based import

---
+++ src/MeshNodes/Transform/ImportFromExcelNodeConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Meshmakers.Octo.Sdk.Common.EtlDataPipeline.Configuration;
+
+namespace Meshmakers.Octo.MeshAdapter.Nodes.Transform;
+
+/// <summary>
+/// Import from excel node
+/// </summary>
+[NodeName("ImportFromExcel", 1)]
+public record ImportFromExcelNodeConfiguration : NodeConfiguration;

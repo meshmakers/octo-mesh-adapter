d755895557a8e3231155ab5298ba34e34e5e6846
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-03-03 16:14:33 +0100

    AB#1856: New: Support of ck type id when using excel import columns

--- src/MeshAdapter/Services/Pipeline/Nodes/Transform/ExcelImport/ColumnContext.cs
+++ src/MeshAdapter/Services/Pipeline/Nodes/Transform/ExcelImport/ColumnContext.cs
@@ -1,11 +1,12 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Meshmakers.Octo.ConstructionKit.Contracts;
+using Newtonsoft.Json.Linq;
 
 namespace Meshmakers.Octo.MeshAdapter.Services.Pipeline.Nodes.Transform.ExcelImport;
 
 
 internal class ColumnContext
 {
-    private record ColumnIndex(string Name, int Index, int Layer);
+    private record ColumnIndex(CkId<CkTypeId> CkTypeId, string AttributePath,  int Index, int Layer);
 
     private readonly List<ColumnIndex> _columnIndexes = [];
 
@@ -14,28 +15,34 @@ internal class ColumnContext
         foreach (var c in columns)
         {
             var column = (JObject)c;
-            var name = column.Value<string>("attributeName");
-            if (name == null)
+            var attributePath = column.Value<string>("attributePath");
+            if (attributePath == null)
                 continue;
+            var ckTypeId = column.Value<string>("ckTypeId") ?? "Basic/TreeNodes";
             var index = column.Value<int>("columnIndex");
             var layer = 1;
             if (column.TryGetValue("layer", StringComparison.InvariantCultureIgnoreCase, out var layerValue))
             {
                 layer = layerValue.Value<int>();
             }
-            _columnIndexes.Add(new(name, index, layer));
+            _columnIndexes.Add(new(ckTypeId, attributePath, index, layer));
         }
     }
 
-    public T? GetValue<T>(JArray row, string name, int layer = 1)
+    public T? GetValue<T>(JArray row, string attributePath, int layer = 1)
     {
-        var index = _columnIndexes.FirstOrDefault(x => x.Name == name && x.Layer == layer)?.Index;
+        var index = _columnIndexes.FirstOrDefault(x => x.AttributePath == attributePath && x.Layer == layer)?.Index;
         return index == null ? default : row[index.Value].Value<T>();
     }
 
-    public IEnumerable<string> GetColumnNames(int layer = 1)
+    public CkId<CkTypeId> GetCkTypeId(JArray row, int layer = 1)
     {
-        return _columnIndexes.Where(x=>x.Layer == layer).Select(x => x.Name);
+        return _columnIndexes.FirstOrDefault(x => x.Layer == layer)?.CkTypeId ?? new CkId<CkTypeId>("Basic/TreeNode");
+    }
+
+    public IEnumerable<string> GetAttributePaths(int layer = 1)
+    {
+        return _columnIndexes.Where(x=>x.Layer == layer).Select(x => x.AttributePath);
     }
     
     public int GetMaxLayer()
--- src/MeshAdapter/Services/Pipeline/Nodes/Transform/ExcelImport/HierarchicalEntity.cs
+++ src/MeshAdapter/Services/Pipeline/Nodes/Transform/ExcelImport/HierarchicalEntity.cs
@@ -2,7 +2,7 @@ using Meshmakers.Octo.ConstructionKit.Contracts;
 
 namespace Meshmakers.Octo.MeshAdapter.Services.Pipeline.Nodes.Transform.ExcelImport;
 
-internal record HierarchicalEntity(string Name, string? ParentName)
+internal record HierarchicalEntity(CkId<CkTypeId> CkTypeId, string Name, string? ParentName)
 {
     public OctoObjectId? RtId { get; set; }
     public List<Tuple<string, string>> Attributes { get; set; } = [];

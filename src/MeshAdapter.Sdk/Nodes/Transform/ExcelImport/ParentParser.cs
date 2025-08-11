0d95f5121acc7ec718eb87673462dd36869b2b62
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-08-11 10:17:09 +0200

    AB#2398: Fix: Column import assignes parent entities correctly now.

--- src/MeshAdapter.Sdk/Nodes/Transform/ExcelImport/ParentParser.cs
+++ src/MeshAdapter.Sdk/Nodes/Transform/ExcelImport/ParentParser.cs
@@ -35,6 +35,6 @@ internal static class ParentNameParser
 
     public static string? ParseLayerBasedName(ColumnContext columnContext, JArray entities, int iLayer)
     {
-        return iLayer == 1 ? null : columnContext.GetValueByPath<string>(entities, "name");
+        return iLayer == 1 ? null : columnContext.GetValueByPath<string>(entities, "name", iLayer - 1);
     }
 }

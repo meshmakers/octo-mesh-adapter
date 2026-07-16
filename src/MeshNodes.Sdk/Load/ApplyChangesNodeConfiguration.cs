2a8af50b54cc36df247735d63850e8851d19d918
Reimar Klammer <reimar.klammer@meshmakers.io>
2026-07-16 21:35:07 +0200

    AB#4404 New: Deprecate ApplyChanges Version 1 node.

--- src/MeshNodes.Sdk/Load/ApplyChangesNodeConfiguration.cs
+++ src/MeshNodes.Sdk/Load/ApplyChangesNodeConfiguration.cs
@@ -5,5 +5,6 @@ namespace Meshmakers.Octo.MeshAdapter.Nodes.Load;
 /// <summary>
 /// Configuration node object for apply changes to the object in mongodb
 /// </summary>
+[NodeDeprecated("Do not use this old version!")]
 [NodeName("ApplyChanges", 1)]
 public record ApplyChangesNodeConfiguration : PathNodeConfiguration;
